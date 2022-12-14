f997c41c547728c63e4c29f8d690765aae8ef180
Andreas Wälchli <andreas.waelchli@me.com>
2022-12-15 00:12:07 +0100

    multi-sample button handling in I/O board
    single-sample processing is vulnerable to sampling errors (interference effects, stray capacitances, etc...).
    
    as a solution we record 3 samples.
    only after 3 samples have been recorded, we process the data.
    Between the 3 samples majority rule is used for each bit. This allows masking of a single outlier within each sample.
    
    untested as of yet

--- code/io_board/src/model/buttons.cpp
+++ code/io_board/src/model/buttons.cpp
@@ -2,17 +2,42 @@
 #include "../networking/eventHandler.h"
 #include "../statusLed.h"
 
-static uint16_t state[4];
+static uint16_t states[4][3];
+static uint16_t last_composites[4];
+static byte sample_count;
 
 void Buttons::update(uint16_t* buffer) {
     for (byte i = 0; i < 4; i++) {
-        uint16_t mask = state[i] ^ buffer[i];
-        state[i] = buffer[i];
-
-        if (mask != 0) {
-            StatusLED::blink();
-            EventHandler::handle(i, state[i], mask);
-        }
+      // record samples
+      states[i][sample_count] = buffer[i];
     }
 
+    sample_count++;
+
+    if (sample_count == 3) {
+      // process samples
+      for (byte i = 0; i < 4; i++) {
+        uint16_t AB = (states[i][0] & states[i][1]);
+        uint16_t AC = (states[i][0] & states[i][2]);
+        uint16_t BC = (states[i][1] & states[i][2]);
+
+        // composite: majority rule: bit is 1 if at least 2 samples are 1.
+        uint16_t composite_sample = AB | AC | BC;
+
+        // mask shows differences between last composite and this one
+        uint16_t mask = last_composites[i] ^ composite_sample;
+
+        // save composite
+        last_composites[i] = composite_sample;
+
+        // if any difference present: process
+        if (mask != 0) {
+          StatusLED::blink();
+          EventHandler::handle(i, composite_sample, mask);
+        }
+      }
+
+      // reset sample count
+      sample_count = 0;
+    }
 }
